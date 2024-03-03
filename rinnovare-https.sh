714eb918e9cd7599871d3e6173d476885e6dd9f5
arduinodenis <90153974+ArduinoDenis@users.noreply.github.com>
2024-03-03 19:34:30 +0100

    Update rinnovare-https.sh

--- rinnovare-https.sh
+++ rinnovare-https.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+# Esegue la simulazione di rinnovo del certificato SSL con Certbot
+
+# Verifica se Certbot è installato
+if ! command -v certbot &>/dev/null; then
+    echo "Error: Certbot non è installato. Installare Certbot prima di eseguire questo script."
+    exit 1
+fi
+
+# Esegue la simulazione di rinnovo del certificato SSL con Certbot in modalità dry-run
 sudo certbot renew --dry-run
